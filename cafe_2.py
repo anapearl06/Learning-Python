05e869867023383c16493cab974f6f468e6b7d35
anapearl06 <shuklaananya762@gmail.com>
2026-06-14 02:52:23 +0530

    updated

---
+++ cafe_2.py
@@ -0,0 +1,10 @@
+snack = input("Enter your preffered snack:").lower()
+
+# print(f"user said: {snack}")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great Choice! We'll serve you Sir! {snack}")
+
+else:
+    print(f"Sorry Sir! We only serve cookies or samosa with tea")
+    
