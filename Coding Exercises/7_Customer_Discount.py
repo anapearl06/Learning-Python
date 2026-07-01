e6bcca70ae5650f503d4b25d82211dea59068c9d
anapearl06 <shuklaananya762@gmail.com>
2026-07-01 16:32:19 +0530

    feat: add restaurant bill and customer discount files

---
+++ Coding Exercises/7_Customer_Discount.py
@@ -0,0 +1,13 @@
+# Customer Discount Eligibility
+
+customer_name = "Amit"
+customer_age = 24
+total_purchase = 1200
+
+# Check discount eligibility
+if total_purchase > 1000:
+    print("Eligible for discount")
+
+# Check senior citizen eligibility
+if customer_age >= 60:
+    print("Senior Citizen")
---
+++ Coding Exercises/8_Restaurant_bill.py
@@ -0,0 +1,6 @@
+bill_amount = 600
+
+if bill_amount > 500:
+    print("You get a free dessert!")
+else:
+    print("No free dessert this time.")
