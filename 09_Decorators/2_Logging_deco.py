b2fb533f588c7c67950e4c42569e6c30f9d225ff
anapearl06 <shuklaananya762@gmail.com>
2026-07-03 16:52:04 +0530

    Add logging decorator

---
+++ 09_Decorators/2_Logging_deco.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+def log_activity(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Calling function '{func.__name__}' with arguments: {args} and keyword arguments: {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"Function '{func.__name__}' returned: {result}")     
+        return result
+    return wrapper
+
+@log_activity
+def add(a, b):
+    return a + b    
+
+result = add(3, 5)
+print(result)
+
