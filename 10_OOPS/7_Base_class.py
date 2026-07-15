07648881f96767442473e37e7a63946338fca8b3
anapearl06 <shuklaananya762@gmail.com>
2026-07-15 18:27:59 +0530

    gggg

---
+++ 10_OOPS/7_Base_class.py
@@ -0,0 +1,23 @@
+class Coffee:
+
+    def __init__(self, type_, strength):
+        self.type = type_
+        self.strength = strength
+
+
+#class LatteCoffee(Coffee):
+#    def __init__(self, type_, strength, spice_level):
+#        self.type = type_
+#        self.strength = strength
+#        self.spice_level = spice_level
+
+#class LatteCoffee(Coffee):
+#    def __init__(self, type_, strength, spice_level):
+#        Coffee.__init__(self, type_, strength)
+#        self.spice_level = spice_level
+
+
+class LatteCoffee(Coffee):
+    def __init__(self, type_, strength, spice_level):
+        super().__init__(self, type_, strength)
+        self.spice_level = spice_level
