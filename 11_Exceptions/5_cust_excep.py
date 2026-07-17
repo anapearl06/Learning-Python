be5ea787ff9c191278ec346d44071fceb2ac51ca
anapearl06 <shuklaananya762@gmail.com>
2026-07-17 18:01:28 +0530

    yessss

--- 11_Exceptions/5_cust_excep.py
+++ 11_Exceptions/5_cust_excep.py
@@ -1,7 +1,7 @@
-def brew_chai(flavour):
-    if flavour not in ["masala", "ginger", "elaichi"]
-        raise ValueError("Unsupported chai flavour")
-    print(f"brewing {flavour} chai...")
-
-brew_chai("mint")
+#def brew_chai(flavour):
+#    if flavour not in ["masala", "ginger", "elaichi"]
+#        raise ValueError("Unsupported chai flavour")
+#    print(f"brewing {flavour} chai...")
+#
+#brew_chai("mint")
 
